abb22afccc1a0e2338c6cec244b43862133e7b3e
Rasmus Hugosson <rasmus.hugosson01@gmail.com>
2026-01-06 15:30:02 +0100

    Add buffer pointer helper

--- app-lib/include/OpenGL.h
+++ app-lib/include/OpenGL.h
@@ -5,6 +5,21 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <cstddef>
+
+namespace ae
+{
+
+/// Converts a byte offset to a void pointer for use with glVertexAttribPointer.
+/// When using VBOs, the pointer parameter is reinterpreted as a byte offset into the buffer.
+inline const void *BufferOffset(std::size_t offset)
+{
+    // NOLINTNEXTLINE(performance-no-int-to-ptr)
+    return reinterpret_cast<const void *>(offset);
+}
+
+} // namespace ae
+
 #ifndef AE_DIST
 
 namespace ae
