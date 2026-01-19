4ad66950d39e77f0773dc07ebfd028cb5195e09e
Rasmus Hugosson <rasmus.hugosson01@gmail.com>
2026-01-19 03:33:58 +0100

    Fix controller debug checks

--- app-lib/src/input/Controller.cpp
+++ app-lib/src/input/Controller.cpp
@@ -11,16 +11,21 @@ bool ae::Controller::IsButtonPressed(int32_t button) const
         return false;
     }
 
-    const unsigned char *pButtons = glfwGetJoystickButtons(GLFW_JOYSTICK_1 + m_Id, nullptr);
+    GLFWgamepadstate state = {};
+    if (!glfwGetGamepadState(GLFW_JOYSTICK_1 + static_cast<int>(m_Id), &state))
+    {
+#ifdef AE_DEBUG
+        AE_LOG(AE_WARNING, "Tried to get button state but controller {} is not connected", m_Id);
+#endif
+        return false;
+    }
 
-    return pButtons[button];
+    return state.buttons[button] == GLFW_PRESS;
 }
 
 bool ae::Controller::IsButtonPressed(ControllerButton button) const
 {
-    const unsigned char *pButtons = glfwGetJoystickButtons(GLFW_JOYSTICK_1 + m_Id, nullptr);
-
-    return pButtons[static_cast<int32_t>(button)];
+    return IsButtonPressed(static_cast<int32_t>(button));
 }
 
 float ae::Controller::GetAxis(int32_t axis) const
@@ -30,26 +35,96 @@ float ae::Controller::GetAxis(int32_t axis) const
         return 0.0f;
     }
 
-    const float *pAxes = glfwGetJoystickAxes(GLFW_JOYSTICK_1 + m_Id, nullptr);
+    GLFWgamepadstate state = {};
+    if (!glfwGetGamepadState(GLFW_JOYSTICK_1 + static_cast<int>(m_Id), &state))
+    {
+#ifdef AE_DEBUG
+        AE_LOG(AE_WARNING, "Tried to get axis state but controller {} is not connected", m_Id);
+#endif
+        return 0.0f;
+    }
 
-    return pAxes[axis];
+    return ApplyDeadzone(state.axes[axis]);
 }
 
 float ae::Controller::GetAxis(ControllerAxis axis) const
 {
-    const float *pAxes = glfwGetJoystickAxes(GLFW_JOYSTICK_1 + m_Id, nullptr);
+    return GetAxis(static_cast<int32_t>(axis));
+}
 
-    return pAxes[static_cast<int32_t>(axis)];
+ae::Vec2 ae::Controller::GetLeftStick() const
+{
+    GLFWgamepadstate state = {};
+    if (!glfwGetGamepadState(GLFW_JOYSTICK_1 + static_cast<int>(m_Id), &state))
+    {
+#ifdef AE_DEBUG
+        AE_LOG(AE_WARNING, "Tried to get left stick but controller {} is not connected", m_Id);
+#endif
+        return {0.0f, 0.0f};
+    }
+
+    return {ApplyDeadzone(state.axes[GLFW_GAMEPAD_AXIS_LEFT_X]),
+            ApplyDeadzone(state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y])};
+}
+
+ae::Vec2 ae::Controller::GetRightStick() const
+{
+    GLFWgamepadstate state = {};
+    if (!glfwGetGamepadState(GLFW_JOYSTICK_1 + static_cast<int>(m_Id), &state))
+    {
+#ifdef AE_DEBUG
+        AE_LOG(AE_WARNING, "Tried to get right stick but controller {} is not connected", m_Id);
+#endif
+        return {0.0f, 0.0f};
+    }
+
+    return {ApplyDeadzone(state.axes[GLFW_GAMEPAD_AXIS_RIGHT_X]),
+            ApplyDeadzone(state.axes[GLFW_GAMEPAD_AXIS_RIGHT_Y])};
+}
+
+ae::Vec2 ae::Controller::GetTriggers() const
+{
+    GLFWgamepadstate state = {};
+    if (!glfwGetGamepadState(GLFW_JOYSTICK_1 + static_cast<int>(m_Id), &state))
+    {
+#ifdef AE_DEBUG
+        AE_LOG(AE_WARNING, "Tried to get triggers but controller {} is not connected", m_Id);
+#endif
+        return {0.0f, 0.0f};
+    }
+
+    // Triggers don't need deadzone - they're linear from -1 to 1
+    return {state.axes[GLFW_GAMEPAD_AXIS_LEFT_TRIGGER],
+            state.axes[GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER]};
+}
+
+float ae::Controller::ApplyDeadzone(float value) const
+{
+    if (std::abs(value) < m_Deadzone)
+    {
+        return 0.0f;
+    }
+
+    // Scale the value so it starts from 0 after the deadzone
+    float sign = (value > 0.0f) ? 1.0f : -1.0f;
+    return sign * (std::abs(value) - m_Deadzone) / (1.0f - m_Deadzone);
 }
 
 std::string ae::Controller::GetName() const
 {
-    std::string name = glfwGetJoystickName(GLFW_JOYSTICK_1 + m_Id);
-
-    return name;
+    const char *pName = glfwGetGamepadName(GLFW_JOYSTICK_1 + static_cast<int>(m_Id));
+    if (pName == nullptr)
+    {
+#ifdef AE_DEBUG
+        AE_LOG(AE_WARNING, "Tried to get name but controller {} is not connected", m_Id);
+#endif
+        return "";
+    }
+    return pName;
 }
 
 bool ae::Controller::IsConnected(uint32_t id)
 {
-    return glfwJoystickPresent(GLFW_JOYSTICK_1 + id) && glfwJoystickIsGamepad(GLFW_JOYSTICK_1 + id);
+    return glfwJoystickPresent(GLFW_JOYSTICK_1 + static_cast<int>(id)) &&
+           glfwJoystickIsGamepad(GLFW_JOYSTICK_1 + static_cast<int>(id));
 }
